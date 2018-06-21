597991d720a48bfe3370c2844208215803821e1c
Philip <phil.tgun@gmail.com>
2018-06-21 21:48:37 +0200

    Updated gaia memory experiment

--- scripts/gaia_exp.py
+++ scripts/gaia_exp.py
@@ -2,30 +2,40 @@ from __future__ import print_function
 from gaia2 import *
 from yaml import safe_dump
 from json import loads
+import sys
 import resource
 
 ds = DataSet()
-data_file = 'data/lowlevel_json_1k.json'
+data_file = 'data/lowlevel_json_10k.json'
 
 counter = 0
-limit = 1000
-step = 10
+limit = 3000
+step = 100
 
 print('Loading data', end='')
 with open(data_file) as infile:
     for line in infile:
-        data = loads(line)
+        try:
+            data = loads(line)
 
-        p = Point()
-        p.loadFromString(safe_dump(data), ['lowlevel.*', 'rhythm.*', 'tonal.*'])
+            p = Point()
+            p.loadFromString(safe_dump(data), ['lowlevel.*', 'rhythm.*', 'tonal.*'])
 
-        p.setName('item' + str(counter))
-        ds.addPoint(p)
+            p.setName('item' + str(counter))
+            ds.addPoint(p)
+
+        except ValueError:
+            pass
+
+        if counter % step == 0:
+            print('.', end='')
+            sys.stdout.flush()
         counter += 1
-        # if counter % step == 0:
-        print('.', end='')
+
         if counter >= limit:
             break
 
+
+
 memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-print('Memory peak: {}'.format(memory))
+print('Memory peak: {} mb'.format(float(memory)/1000))
