849e5ecaf6ad8dd4563f0085133075046ad3c906
Philip Tovstogan <phil.tgun@gmail.com>
2018-06-26 01:45:54 +0200

    Added some plots

--- scripts/cube_time.py
+++ scripts/cube_time.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def func(x, a, b):
-    return a*x + b
+def func(x, a, b, c):
+    return a*np.log(x + b) + c
 
 
-sizes = np.array([100, 300, 1000, 3000, 10000])
-memory = np.array([25.3, 29.5, 44.5, 95.9, 266.7])
+sizes = np.array([1000, 10_000, 100_000, 1_000_000, 8_000_000])
+time = np.array([0.023, 0.03, 0.16, 1.1, 2.9])
 
-popt, pcov = curve_fit(func, sizes, memory, p0=(1, 0))
-plt.loglog(sizes, memory, linestyle=' ', marker='o')
+popt, pcov = curve_fit(func, sizes, time, p0=(1, 0, 0))
+plt.loglog(sizes, time, linestyle=' ', marker='o')
 plt.loglog(sizes, func(sizes, *popt))
 est = func(10_000_000, *popt)
 print('Estimated usage at 10m: {}'.format(est))
@@ -21,5 +21,5 @@ plt.grid(which='major', linestyle='-')
 plt.title('Gaia memory usage')
 plt.xlabel('Number of points')
 plt.ylabel('Memory usage (MB)')
-plt.savefig('gaia.png', bbox_inches='tight')
+plt.savefig('cube.png', bbox_inches='tight')
 plt.show()
